1e1338ae7a0a29b403f631f3d3c4e75333993dc5
Tony <tony@gmail.com>
2016-06-06 00:42:47 +0800

    first comit base UI

---
+++ app/src/main/java/com/example/android/launcherfullmirror/main_activity.java
@@ -0,0 +1,13 @@
+package com.example.android.launcherfullmirror;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class main_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_page_2);
+    }
+}
